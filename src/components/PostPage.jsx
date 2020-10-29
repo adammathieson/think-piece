9ce0af624a79098e0709bd96e8a3f1ab26fcfb16
Adam Mathieson <mathieson1222@gmail.com>
2020-10-28 23:14:03 -0700

    route to a postPage for each post

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -2,15 +2,50 @@ import React, { Component } from 'react'
 
 import Post from './Post'
 import Comments from './Comments'
-import { Firestore } from '../firebase'
+import { firestore } from '../firebase'
 import { collectIdsAndDocs } from '../utilities'
+
+import { withRouter } from 'react-router-dom'
 
 class PostPage extends Component {
     state = { post: null, comments: []}
 
+    get postId() {
+        return this.props.match.params.id
+    }
+
+    get postRef() {
+        return firestore.doc(`posts/${this.postId}`)
+    }
+
+    get commentRef() {
+        return this.postRef.collection('comments')
+    }
+
+    unsubscribeFromPost = null
+    unsubscribeFromComments = null
+
+    componentDidMount = async () => {
+        this.unsubscribeFromPost = this.postRef.onSnapShot(snapshot => {
+            const post = collectIdsAndDocs(snapshot)
+            this.setState({ post })
+        })
+        this.unsubscribeFromComments = this.commentRef.onSnapShot(snapshot => {
+            const comments = snapshot.docs.map(collectIdsAndDocs)
+            this.setState({ comments })
+        })
+    }
+
+    componentWillUnmount = () => {
+        this.unsubscribeFromPost()
+        this.unsubscribeFromComments()
+    }
+
     render() {
-        return <div>Post Page!</div>
+        const { match } = this.props
+        console.log(this.props)
+        return <div>Post Page! {match.params.id}</div>
     }
 }
 
-export default PostPage
+export default withRouter(PostPage)
