196d89b7b7754757cd6fc22e9e09034c29d3e6ae
Adam Mathieson <mathieson1222@gmail.com>
2020-10-29 22:48:58 -0700

    fixes bug with onSnapshot !onSnapShot

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -9,42 +9,52 @@ import { withRouter } from 'react-router-dom'
 
 class PostPage extends Component {
     state = { post: null, comments: []}
-
+    
     get postId() {
         return this.props.match.params.id
     }
-
+    
     get postRef() {
         return firestore.doc(`posts/${this.postId}`)
     }
-
+    
     get commentRef() {
         return this.postRef.collection('comments')
     }
-
+    
     unsubscribeFromPost = null
     unsubscribeFromComments = null
-
+    
     componentDidMount = async () => {
-        this.unsubscribeFromPost = this.postRef.onSnapShot(snapshot => {
+        this.unsubscribeFromPost = this.postRef.onSnapshot(snapshot => {
             const post = collectIdsAndDocs(snapshot)
             this.setState({ post })
         })
-        this.unsubscribeFromComments = this.commentRef.onSnapShot(snapshot => {
+        this.unsubscribeFromComments = this.commentRef.onSnapshot(snapshot => {
             const comments = snapshot.docs.map(collectIdsAndDocs)
             this.setState({ comments })
         })
     }
-
+    
     componentWillUnmount = () => {
         this.unsubscribeFromPost()
         this.unsubscribeFromComments()
     }
-
+    
     render() {
-        const { match } = this.props
-        console.log(this.props)
-        return <div>Post Page! {match.params.id}</div>
+        const { post, comments } = this.state
+        // const { match } = this.props
+        // console.log(this.props)
+        return (
+            <section>
+                {/* { post && <Post {...post} />}
+                <Comments
+                    comments={comments}
+                    postId={post.id}
+                    onCreate={() => {}}
+                /> */}
+            </section>
+        )
     }
 }
 
